d8b5aa165469acd743e66016e06741607582fc84
NotVndtta <thenotalexey192@gmail.com>
2023-08-05 00:05:56 +0300

    Brands and start cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Header from './components/Header/Header.jsx';
+import Arrivals from './components/arrivals/Arrivals.jsx';
+import Brands from './components/brands/Brands.jsx';
 import Promo from './components/promo/Promo.jsx';
 function App() {
   return (
@@ -6,6 +8,8 @@ function App() {
       <h1> powered by Dust </h1>
       <Header />
       <Promo />
+      <Brands />
+      <Arrivals />
     </div>
   );
 }
---
+++ src/components/arrivals/Arrivals.jsx
@@ -0,0 +1,13 @@
+import "./arrivals.css"
+
+const Arrivals = () => {
+    return (<section className="arrivals">
+    <div className="container">
+        <div className="arrivals_header"></div>
+            <h2 className="title-2">NEW ARRIVALS</h2>
+        <div className="arrivals_cards"></div>
+    </div>
+    </section> );
+}
+ 
+export default Arrivals;
